5885152d3bfc160422d033937dfef6b9f390f57a
saegusa <kiennd010498@gmail.com>
2022-08-20 00:22:57 +0900

    add command, key bindings

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,46 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+function runAll() {
+  console.log('run All');
+}
+
+function runFile() {
+  console.log('run File');
+}
+
+function runLine() {
+  console.log('run Line');
+}
+
+export function activate(context: vscode.ExtensionContext) {	
 	console.log('Congratulations, your extension "rspec-runner" is now active!');
+
+  let runAllDisposable = vscode.commands.registerCommand('rspec-runner.runAll', () => {
+    vscode.window.showInformationMessage('runAll');
+  });
+  context.subscriptions.push(runAllDisposable);
+
+  let runFileDisposable = vscode.commands.registerCommand('rspec-runner.runFile', () => {
+    vscode.window.showInformationMessage('runFile');
+  });
+  context.subscriptions.push(runFileDisposable);
+
+  let runLineDisposable = vscode.commands.registerCommand('rspec-runner.runLine', () => {
+    vscode.window.showInformationMessage('runLine');
+  });
+  context.subscriptions.push(runLineDisposable);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rspec-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rspec-runner!');
-	});
+	// let disposable = vscode.commands.registerCommand('rspec-runner.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from rspec-runner!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
