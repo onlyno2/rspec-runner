4cbed32d5d051ddb2a0a22d9e58a355584974635
saegusa <kiennd010498@gmail.com>
2022-08-21 21:22:00 +0900

    add run current rspec file function

--- src/extension.ts
+++ src/extension.ts
@@ -35,8 +35,39 @@ function runAll() {
   execCommand('bundle exec rspec');
 }
 
+function getFilePath(): string {
+  if (!vscode.window.activeTextEditor) {
+    return '';
+  }
+
+  return vscode.window.activeTextEditor?.document.uri.path;
+}
+
+function getWorkspacePath(): string {
+  if (!vscode.workspace.workspaceFolders) {
+    return '';
+  }
+
+  const workspacePaths: string[] = vscode.workspace.workspaceFolders.map(workspaceFolder => workspaceFolder.uri.path);
+  return workspacePaths.find(path => getFilePath().includes(path)) || '';
+}
+
 function runFile() {
-  console.log('run File');
+  const filePath = getFilePath();
+  const workspacePath = getWorkspacePath();
+
+  if (filePath === '' || workspacePath === '') {
+    vscode.window.showErrorMessage('No file');
+    return;
+  }
+
+  if (!/_spec.rb/.test(filePath)) {
+    vscode.window.showErrorMessage('Not rspec file');
+    return;    
+  }
+
+  const file = filePath.replace(`${workspacePath}/`, "");
+  execCommand(`bundle exec rspec ${file}`);
 }
 
 function runLine() {
@@ -52,7 +83,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(runAllDisposable);
 
   let runFileDisposable = vscode.commands.registerCommand('rspec-runner.runFile', () => {
-    vscode.window.showInformationMessage('runFile');
+    clearTerminal()?.then(() => runFile());
   });
   context.subscriptions.push(runFileDisposable);
 
